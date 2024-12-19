214fe2a350c60dc9003186cb148fc5bbe436f6f8
ped123456 <guhss@hotmail.com>
2024-12-19 05:57:02 -0300

    projeto.2
    em construção

---
+++ Programadores.2/script.js
@@ -0,0 +1,13 @@
+function noturno(){
+    
+    const titulo = document.querySelector("h1");
+    const subTitulo = document.querySelector("h2");
+    const links = document.querySelector("a");
+    const texto = document.querySelector("p");
+
+    document.body.style.backgroundColor="black";
+    titulo.style.color = "white";
+    subTitulo.style.color = "white";
+    links.style.color = "white";
+    texto.style.color = "white";
+}
