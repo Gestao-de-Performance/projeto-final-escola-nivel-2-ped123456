dfc87198fdacda55c1d78254de8dbc2f34e89fca
ped123456 <guhss@hotmail.com>
2024-12-19 12:23:38 -0300

    projeto.2
    função modo noturno

--- Programadores.2/script.js
+++ Programadores.2/script.js
@@ -1,13 +1,64 @@
+const titulo = document.querySelector("h1");
+const subTitulos = document.querySelectorAll("h2");
+const subTituloskills = document.querySelectorAll("h3");
+
+const links = document.querySelectorAll("a");
+const textos = document.querySelectorAll("p");
+
+const corTitulo = titulo.style.color;
+const corSubtitulo = subTitulos[0].style.color;
+const corSubtituloskill = subTituloskills[0].style.color;
+const corLink = links[0].style.color;
+const corTexto = textos[0].style.color;
+
+const diurno = true;
+
 function noturno(){
     
-    const titulo = document.querySelector("h1");
-    const subTitulo = document.querySelector("h2");
-    const links = document.querySelector("a");
-    const texto = document.querySelector("p");
-
+    if (diurno)
+    
     document.body.style.backgroundColor="black";
     titulo.style.color = "white";
-    subTitulo.style.color = "white";
-    links.style.color = "white";
-    texto.style.color = "white";
+
+    textos.forEach(texto => {
+        texto.style.color = "white";
+    })
+    
+
+    subTitulos.forEach(subTitulo => {
+        subTitulo.style.color = "white";   
+    });
+
+    subTituloskills.forEach(subTituloskill => {
+        subTituloskill.style.color = "white";   
+    });
+    
+    console.log(links);
+
+    links.forEach(link => { 
+        link.style.color = "white";
+    });
+    
+    else {
+        document.body.style.backgroundColor = "white";
+
+        titulo.style.color = corTitulo;
+
+        subTitulos.forEach(subTitulo =>{
+            subTitulo.style.color = corSubTitulo;
+        })
+
+        subTituloskills.forEach(subTituloskill =>{
+            subTituloskill.style.color = corSubTituloskill; 
+        })
+
+        links.forEach(link =>{
+            link.style.color = "corLinks";
+        })
+
+        textos.forEach(texto =>{
+            texto.style.color = "corTexto";
+        })
+    }
+    
 }
