d7df1ef50e47993f3db470a6593343125257d677
ped123456 <guhss@hotmail.com>
2024-12-19 23:22:15 -0300

    projeto.2
    concluido modo noturno

--- Programadores.2/script.js
+++ Programadores.2/script.js
@@ -6,16 +6,16 @@ const links = document.querySelectorAll("a");
 const textos = document.querySelectorAll("p");
 
 const corTitulo = titulo.style.color;
-const corSubtitulo = subTitulos[0].style.color;
-const corSubtituloskill = subTituloskills[0].style.color;
+const corSubTitulo = subTitulos[0].style.color;
+const corSubTituloskill = subTituloskills[0].style.color;
 const corLink = links[0].style.color;
 const corTexto = textos[0].style.color;
 
-const diurno = true;
+let diurno = true;
 
 function noturno(){
     
-    if (diurno)
+    if (diurno) {
     
     document.body.style.backgroundColor="black";
     titulo.style.color = "white";
@@ -33,32 +33,36 @@ function noturno(){
         subTituloskill.style.color = "white";   
     });
     
-    console.log(links);
-
+    
     links.forEach(link => { 
         link.style.color = "white";
     });
+
+        return diurno = false;
     
-    else {
+    }else {
         document.body.style.backgroundColor = "white";
 
         titulo.style.color = corTitulo;
 
+        textos.forEach(texto =>{
+            texto.style.color = corTexto;
+        });
+
+
         subTitulos.forEach(subTitulo =>{
             subTitulo.style.color = corSubTitulo;
-        })
+        });
 
         subTituloskills.forEach(subTituloskill =>{
             subTituloskill.style.color = corSubTituloskill; 
-        })
+        });
 
         links.forEach(link =>{
-            link.style.color = "corLinks";
-        })
+            link.style.color = corLink;
+        });
 
-        textos.forEach(texto =>{
-            texto.style.color = "corTexto";
-        })
+       
     }
     
 }
